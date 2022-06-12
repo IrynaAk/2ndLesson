8a624c36db080a2f0eb7b1829ca631efd0fba466
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 16:29:35 +0300

    3 digits number without 2nd digit

---
+++ Task01(again)/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+using System;
+using static System.Console;
+
+Clear();
+
+int number=new Random().Next(100,1000);
+WriteLine(number);
+Write($"Numer without 2nd digit = {number/100}{number%10}");
+
