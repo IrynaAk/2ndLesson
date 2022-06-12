2e2be266a414b1dc0474d20297286ca7e45af90c
Iryna_Akunevich <akunevichirina@mail.ru>
2022-06-12 16:21:54 +0300

    Check if the number is a multiple of 7 and 23

---
+++ Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Type the first number ");
+int number = Convert.ToInt32(ReadLine());
+
+string answer = number%7 == 0 && number%23 == 0?"Yes":"No";
+Write(answer);
